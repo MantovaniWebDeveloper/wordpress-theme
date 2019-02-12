b407e17bd943f8187c512a343a12a5c06b2196ad
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-02-12 16:21:29 +0100

    configuratp il functions.php, inserito bulma come framework

--- header.php
+++ header.php
@@ -4,7 +4,11 @@
     <meta charset="<?php bloginfo("charset"); ?>">
     <title><?php bloginfo("title"); ?></title>
     <meta name="description" content="<?php bloginfo("description"); ?>">
-
     <?php wp_head(); ?>
   </head>
   <body>
+    <header>
+      <div class="container">
+        
+      </div>
+    </header>
