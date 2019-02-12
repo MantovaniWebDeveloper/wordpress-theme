54c05edc8cf15e4383f8579e2400c604f2098a82
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-02-12 16:31:22 +0100

    aggiunti i controlli sulle funzioni

--- functions.php
+++ functions.php
@@ -1,33 +1,38 @@
 <?php
 
   /*CONFIGURAZIONI BASE DEL TEMA*/
-  function wt_setup_theme(){
-    //attivo il supporto dei titoli
-    add_theme_support("title-tag");
-    //attivo il supporto per le imagini in articoli e pagine
-    add_theme_support("post_thumbnails");
-    //dimensioni per le imagini in articoli e pagine
-    add_image_size('wt_big',1400,600,true);
-    add_image_size('wt_quad',600,600,true);
-    add_image_size('wt_single',800,500,true);
+  if(! function_exists()) {
+
+    function wt_setup_theme(){
+      //attivo il supporto dei titoli
+      add_theme_support("title-tag");
+      //attivo il supporto per le imagini in articoli e pagine
+      add_theme_support("post_thumbnails");
+      //dimensioni per le imagini in articoli e pagine
+      add_image_size('wt_big',1400,600,true);
+      add_image_size('wt_quad',600,600,true);
+      add_image_size('wt_single',800,500,true);
 
 
-    //attivazione custom menu
-    register_nav_menus(array(
-      'header' => 'topMenuHeader',
-    ));
+      //attivazione custom menu
+      register_nav_menus(array(
+        'header' => 'topMenuHeader',
+      ));
 
 
-  }
+    }
 
+}
   add_action('after_setup_theme', 'wt_setup_theme');
   //***************************************************/
 
   /*INCLUSIONE STILI*/
+  if(! function_exists()) {
 
-  function wt_style(){
-    wp_enqueue_style('bulma-css', get_template_directory_uri().'/css/style.css');
-  }
+    function wt_style(){
+      wp_enqueue_style('bulma-css', get_template_directory_uri().'/css/style.css');
+    }
+}
 
   add_action('wp_enqueue_scripts', 'wt_style');
 
