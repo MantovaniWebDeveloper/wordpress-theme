cffcf6f40b866fd393b8433555671bacbbe6f65c
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-02-12 15:51:15 +0100

    settate header con le funzion wp necessarie

--- header.php
+++ header.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="it" dir="ltr">
+<html <?php language_attributes(); ?>>
   <head>
-    <meta charset="utf-8">
+    <meta charset="<?php bloginfo("charset"); ?>">
     <title><?php bloginfo("title"); ?></title>
+    <meta name="description" content="<?php bloginfo("description"); ?>">
+
+    <?php wp_head(); ?>
   </head>
   <body>
-
-  </body>
-</html>
